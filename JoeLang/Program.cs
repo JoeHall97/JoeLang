3a0526f6ce53ea9310b3df1a74dd672e66dfbadb
Joseph Hall <josephdahall@hotmail.co.nz>
2024-01-05 16:08:38 +1300

    Added file input, updated to dotnet 8.

--- JoeLang/Program.cs
+++ JoeLang/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using JoeLang.AST;
+using JoeLang.Evaluator;
+using JoeLang.Lexer;
+using JoeLang.Object;
+using JoeLang.Parser;
 using JoeLang.REPL;
 
 namespace JoeLang;
@@ -7,11 +13,41 @@ class JoeLang
 {
     public static void Main(string[] args)
     {
-        var userName = Environment.UserName;
+        if (args.Length == 0) {
+            var userName = Environment.UserName;
 
-        Console.WriteLine($"Hello {userName}! This is the Joe programming language.");
-        Console.WriteLine("Please type in commands.");
+            Console.WriteLine($"Hello {userName}! This is the Joe programming language.");
+            Console.WriteLine("Please type in commands.");
 
-        JoeREPL.Start();
+            JoeREPL.Start();
+            return;
+        }
+        EvaluateFile(args[0]);
+    }
+
+    private static void EvaluateFile(string filename) 
+    {
+        string input = ReadFile(filename);
+        JoeEnvironment environment = new();
+        JoeEvaluator evaluator = new();
+
+        JoeLexer lexer = new(input);
+        DescentParser parser = new(lexer);
+        JoeProgram program = parser.ParseProgram();
+
+        if (parser.Errors.Length > 0)
+        {
+            Console.WriteLine("Encountered parser errors:");
+            foreach (var error in parser.Errors)
+                Console.WriteLine('\t' + error + '\n');
+        }
+
+        var evaluated = evaluator.Evaluate(program,environment);
+    }
+
+    private static string ReadFile(string filename) 
+    {
+        StreamReader sr = new(filename);
+        return sr.ReadToEnd();
     }
 }
